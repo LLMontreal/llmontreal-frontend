e14cd4dc89abcf607dfc893a862a17deb466ba14
Nilton Guilherme Pacheco Dos Santos <niltong.pacheco@gmail.com>
2025-10-27 08:11:32 -0300

    Refactor: Rename modules directory to features

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080/api',
   appName: 'LL Montreal',
   version: '1.0.0',
   features: {
