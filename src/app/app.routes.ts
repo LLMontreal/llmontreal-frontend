8dee53509c01a08f3ed77b959180bf4118bad906
Izabela Gomes <izgpbela.dev@gmail.com>
2025-10-29 00:28:22 -0300

    feat: define application routes for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: DashboardComponent },
+	{ path: 'dashboard', redirectTo: '', pathMatch: 'full' },
+	{ path: 'upload', loadComponent: () => import('./components/upload/upload.component').then(m => m.UploadComponent) },
+	{ path: 'config', loadComponent: () => import('./components/config/config.component').then(m => m.ConfigComponent) },
+	{ path: '**', redirectTo: '' }
+];
+
