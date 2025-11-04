c713ee52782243dfaf0ee6de7a20650180a4965b
Izabela Gomes <izgpbela.dev@gmail.com>
2025-11-04 16:12:25 -0300

    chore(router): make UploadDocument the default route (landing page)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { UploadDocumentComponent } from './components/upload-document/upload-document.component';
 
 export const routes: Routes = [
 
-	{ path: '', redirectTo: 'upload', pathMatch: 'full' },
+	{ path: '', component: UploadDocumentComponent, pathMatch: 'full' },
 	{ path: 'dashboard', component: DashboardComponent },
-	{ path: 'upload', component: DashboardComponent },
+	{ path: 'upload-document', component: UploadDocumentComponent },
 	{ path: '**', redirectTo: '' }
 ];
 
