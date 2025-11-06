311de6399b48ec6091c80015896b9d06a44b4a9d
yararosasilva <yararosadasilva19@gmail.com>
2025-11-06 11:03:26 -0300

    adiciona variável de ambiente, inject(), @if e uploadSub?.unsubscribe();

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -1,20 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DocumentService {
-  private baseUrl = 'http://localhost:8080/api/documents';
+  private http = inject(HttpClient);
+  private baseUrl = `${environment.apiUrl}/documents`;
 
-  constructor(private http: HttpClient) {}
-
-  uploadDocument(file: File): Observable<HttpEvent<any>> {
+   uploadDocument(file: File): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
     formData.append('file', file);
 
-    
     return this.http.post<any>(this.baseUrl, formData, {
       reportProgress: true,
       observe: 'events',
