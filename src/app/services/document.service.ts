cf79094a3c02bcf3d5d1592a38a2fe52d7231e19
yararosasilva <yararosadasilva19@gmail.com>
2025-11-07 17:07:55 -0300

    adiciona model DocumentUploadResponse e zip

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { DocumentUploadResponse } from '../models/document-upload-response.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,11 +11,11 @@ export class DocumentService {
   private http = inject(HttpClient);
   private baseUrl = `${environment.apiUrl}/documents`;
 
-   uploadDocument(file: File): Observable<HttpEvent<any>> {
+   uploadDocument(file: File): Observable<HttpEvent<DocumentUploadResponse>> {
     const formData: FormData = new FormData();
     formData.append('file', file);
 
-    return this.http.post<any>(this.baseUrl, formData, {
+    return this.http.post<DocumentUploadResponse>(this.baseUrl, formData, {
       reportProgress: true,
       observe: 'events',
     });
