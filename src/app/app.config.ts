7cd5054a55b1349f2577f0c4155e5a7b3a1ac5fe
Izabela Gomes <izgpbela.dev@gmail.com>
2025-11-10 14:10:08 -0300

    refactor: remove unused error listener provider and enhance paginator configuration in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    {
+      provide: MatPaginatorIntl,
+      useFactory: () => {
+        const paginator = new MatPaginatorIntl();
+        paginator.itemsPerPageLabel = 'Itens por página';
+        return paginator;
+      }
+    }
   ]
 };
-
